ddff43de06c7672d5c1b67b89e669be3e0424ba5
Herbert Tung <eu03@18m.dev>
2021-06-25 17:02:35 +0800

    init

---
+++ content.js
@@ -0,0 +1,55 @@
+​/admin​/login
+​/admin​/merchant
+​/admin​/merchant
+​/admin​/merchant​/{merchantId}
+​/admin​/subAdmin
+​/admin​/subAdmin
+​/admin​/subAdmin​/{adminId}
+​/admin​/subAdmin​/role​/{adminId}
+​/admin​/subAdmin​/password​/{adminId}
+​/admin​/subAdmin​/status​/{adminId}
+​/admin​/permission
+​/admin​/role
+​/admin​/role​/{roleId}
+​/admin​/pendingWithdraw
+PUT ​/admin​/pendingWithdraw​/{pendingWithdrawId}
+​/admin​/pendingWithdrawHistory
+​/admin​/pendingConfirmAPISetting
+​/admin​/pendingConfirmAPISetting​/{pendingId}
+​/admin​/server​/list
+​/admin​/fee​/withdrawalFee
+​/merchant​/login
+​/merchant​/initalizeWallet
+​/merchant​/subAdmin
+​/merchant​/subAdmin​/{merchantAdminId}
+​/merchant​/subAdmin​/password​/{merchantAdminId}
+​/merchant​/subAdmin​/status​/{merchantAdminId}
+​/merchant​/permission
+​/merchant​/role
+​/merchant​/role​/{roleId}
+​/merchant​/role​/{roleId}
+​/merchant​/role​/{roleId}
+​/merchant​/transfer
+​/merchant​/withdraw
+​/admin​/fee​/withdrawalFee
+Update withdraw fee in ETH
+testnet
+
+
+GET
+​/testnet​/asset​/faucet
+Get faucet currency config
+
+POST
+​/testnet​/asset​/faucet
+Faucet certain currency to target wallet address at Rinkeby
+
+Schemas
+GeneralRequest
+GeneralResponse
+Permission
+MerchantDetails
+MerchantAdmin
+ServerReturnCode
+CurrencySymbol
+Online validator badge
